3ad37ff02f0ec1e6e9b1ad0b55d6c78e2961500f
Santailha <vendas@santailha.com.br>
2025-08-26 15:58:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,15 +7,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
         const itemName = document.getElementById('itemName').value;
         const itemQuantity = parseInt(document.getElementById('itemQuantity').value);
-        const itemPrice = parseFloat(document.getElementById('itemPrice').value);
-        const totalValue = itemQuantity * itemPrice;
 
         const newRow = `
             <tr>
                 <td>${itemName}</td>
                 <td>${itemQuantity}</td>
-                <td>R$ ${itemPrice.toFixed(2)}</td>
-                <td>R$ ${totalValue.toFixed(2)}</td>
                 <td><button class="btn-delete">Remover</button></td>
             </tr>
         `;
